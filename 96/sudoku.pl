a6aa52cd33a80542a52fcd190511ef2f7e4c188a
Eginhard <eginhard@server.fake>
2013-10-02 17:21:10 +0100

    Problem 96: work-in-progress

--- 96/sudoku.pl
+++ 96/sudoku.pl
@@ -5,4 +5,9 @@ sudoku(Rows) :-
 	maplist(all_distinct, Rows),
 	transpose(Rows, Columns),
 	maplist(all_distinct, Columns),
-	
+
+
+blocks([], [], []).
+blocks([A,B,C|Rest1], [D,E,F|Rest2], [G,H,I|Rest3]) :-
+	all_distinct([A,B,C,D,E,F,G,H,I]),
+	blocks(Rest1, Rest2, Rest3).
