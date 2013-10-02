67ea7795dd22c3dc0e3d1ef5cd92d7963ec30eaf
Eginhard <eginhard@server.fake>
2013-10-02 20:48:45 +0100

    Problem 96: almost there

--- 96/sudoku.pl
+++ 96/sudoku.pl
@@ -5,7 +5,9 @@ sudoku(Rows) :-
 	maplist(all_distinct, Rows),
 	transpose(Rows, Columns),
 	maplist(all_distinct, Columns),
-
+	Rows = [A,B,C,D,E,F,G,H,I],
+	blocks(A,B,C), blocks(D,E,F), blocks(G,H,I),
+	maplist(label, Rows).
 
 blocks([], [], []).
 blocks([A,B,C|Rest1], [D,E,F|Rest2], [G,H,I|Rest3]) :-
